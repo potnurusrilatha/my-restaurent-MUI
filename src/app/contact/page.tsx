e74cfcad11e9aa7b8b2ca404ff6e9444b2ae8025
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2025-05-21 14:12:01 +0200

    added recipecard

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,71 @@
+'use client'
 
-const contact = () => {
+import { Container,Box,Paper,Typography, Button}  from "@mui/material"
+import { RecipeType } from "../types/recipe"
+import { useState } from "react"
+import RecipeCard from "../../component/RecipeCard"
+
+const RecipePage =() => {
+  const [recipe, setRecipe] = useState<RecipeType | null>(null)
+
+  const fetchRecipe = async():Promise<void> => {
+      const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const data = await response.json();
+    const recipeData = data.meals[0];
+    const savedIngredients:string[] = []
+
+    const keys = Object.keys(recipeData).filter(item => item.includes("strIngredient"));
+
+    for(let i=0; i< keys.length; i++) {
+      if(recipeData[keys[i]]) {
+        savedIngredients.push(
+          recipeData[keys[i]] + " - " + recipeData[`strMeasures${i + 1}`])
+
+      }else{
+        break;
+      }
+    }
+    console.log(savedIngredients)
+
+    let recipeToSave ={
+     name: recipeData.strMeal,
+      category: recipeData.strCategory,
+      area: recipeData.strArea,
+      image:recipeData.strMealThumb,
+      video:recipeData.strYoutube,
+      instructions: recipeData.strInstructions,
+      ingredients: savedIngredients
+      
+    } 
+    setRecipe(recipeToSave)
+  }
+
+
+
+
   return (
-    <div>
-      <p>Contact page</p>
-    </div>
+   
+   <Container sx={{
+       mt:4
+       }}
+        maxWidth="sm">
+      <Paper elevation={12}>
+        <Box sx={{
+         p: 2,
+         textAlign: 'center',
+         backgroundColor: 'yellowgreen' }}>
+      <Typography variant="h1" color="error" gutterBottom
+      sx={{
+        fontsize: {
+          xs: "3rem",
+          sm: "4rem"
+        }
+      }}> Dinner Meal</Typography>
+      <Button variant="contained" onClick={fetchRecipe}>Find a meal!</Button>
+      {recipe && <RecipeCard{...recipe} />}
+  </Box></Paper></Container>
+    
   )
-}
 
-export default contact
+}
+export default RecipePage
---
+++ src/component/RecipeCard/index.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+// import { TabContext } from '@mui/lab';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+
+
+// Update the import path below to the correct relative path where RecipeType is defined.
+// For example, if RecipeType is in src/types/recipe.ts, use:
+import RecipeType from "@/utils/recipe"
+// If the file does not exist, create it and define RecipeType there.
+ import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+
+ 
+
+ 
+export default function RecipeReviewCard({name,id,image,category,area, ingredients, instructions}:RecipeType) {
+
+  const [value, setValue] = React.useState('1');
+ 
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  }
+ 
+  return (
+
+    <Card elevation={12} sx={{ maxWidth: "sm" , my:4}}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "seashell",color: "brown" }} aria-label="recipe">
+          SP
+          </Avatar>
+        }
+       
+        title={name}
+        subheader={`${category} - ${area}`}
+      />
+      <CardMedia
+        component="img"
+        image={image}
+        alt={name}
+      />
+      <CardContent>
+          <Box sx={{ width: '100%', typography: 'body1' }}>
+  <TabContext value={value}>
+  <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+  <TabList onChange={handleChange} aria-label="lab API tabs example">
+  <Tab label="Item One" value="1" />
+  <Tab label="Item Two" value="2" />
+  <Tab label="Item Three" value="3" />
+  </TabList>
+  </Box>
+  <TabPanel value="1">Item One</TabPanel>
+  <TabPanel value="2">Item Two</TabPanel>
+  <TabPanel value="3">Item Three</TabPanel>
+  </TabContext>
+</Box>
+      </CardContent>
+    </Card>
+  );
+}
